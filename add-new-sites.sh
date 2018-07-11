bd6e458df5825dbd47c59590adf8abc653ff119e
Alec Muffett <alec.muffett@gmail.com>
2018-07-11 00:54:14 +0000

    work in progress, new server

--- add-new-sites.sh
+++ add-new-sites.sh
@@ -22,7 +22,8 @@ while read category scheme onion_address proof_url title ; do
     echo $scheme://$onion_address/ >$dir/urls # add more by hand, later
 done <<EOF
 securedrop-for-organisations http lzpczap7l3zxu7zv.onion https://www.icij.org/securedrop ICIJ / International Consortium of Investigative Journalists
-tech-and-software https hzwjmjimhr7bdmfv2doll4upibt5ojjmpo3pbp5ctwcg37n3hyk7qzid.onion https://onioncontainers.com/ Ablative Hosting (proof in onion ssl cert)
+tech-and-software https hzwjmjimhr7bdmfv2doll4upibt5ojjmpo3pbp5ctwcg37n3hyk7qzid.onion proof-in-ssl-cert Ablative Hosting
 civil-society-and-community https xychelseaxqpywe4.onion https://twitter.com/xychelsea/status/989539441978040321 Chelsea Manning for US Senate
-news-and-media https p53lf57qovyuvwsc6xnrppyply3vtqm7l6pcobkmyqsiofyeznfu5uqd.onion https://www.propublica.org ProPublica (proof in onion ssl cert)
+news-and-media https p53lf57qovyuvwsc6xnrppyply3vtqm7l6pcobkmyqsiofyeznfu5uqd.onion proof-in-ssl-cert ProPublica
+securedrop-for-organisations https y27vf7g2ce5g3fnl.onion proof-in-ssl-cert Aftonbladet
 EOF
