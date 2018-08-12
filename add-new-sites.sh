fea367d1598911f06233e4f99eb2d619dc694e14
Alec Muffett <alec.muffett@gmail.com>
2018-08-12 22:06:32 +0000

    qubes and whonix fixes

--- add-new-sites.sh
+++ add-new-sites.sh
@@ -23,14 +23,6 @@ while read category scheme onion_address proof_url title ; do
     echo $proof_url >$dir/proof
     echo $scheme://$onion_address/ >$dir/urls # add more by hand, later
 done <<EOF
-news-and-media https bfnews3u2ox4m4ty.onion proof-in-ssl-cert BuzzFeed News
-
-tech-and-software http lldan5gahapx5k7iafb3s4ikijc4ni7gx5iywdflkba5y2ezyg6sjgyd.onion https://onionshare.org/ OnionShare
-
-tech-and-software http sik5nlgfc5qylnnsr57qrbm64zbdx6t4lreyhpon3ychmxmiem7tioad.onion http://sik5nlgfc5qylnnsr57qrbm64zbdx6t4lreyhpon3ychmxmiem7tioad.onion/ Qubes OS
-
-tech-and-software http dxsj6ifxytlgq33k.onion https://hardenedbsd.org/article/shawn-webb/2017-03-11/hardenedbsd-through-tor-hidden-service Hardened BSD
-
-tech-and-software http 3jkjhrvkdbdkqisnwhdpe4afh2j2g3suhsfcewiemsyk5ecd6gadmxyd.onion https://hardenedbsd.org/article/shawn-webb/2017-03-11/hardenedbsd-through-tor-hidden-service Hardened BSD
+tech-and-software http kkkkkkkkkk63ava6.onion https://www.qubes-os.org/news/2018/01/23/qubes-whonix-next-gen-tor-onion-services/ Whonix
 
 EOF
