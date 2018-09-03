0629e82e1e6a2fcc13d8f5c0ac2e1f0ba511ea52
Alec Muffett <alec.muffett@gmail.com>
2018-09-03 06:49:40 +0000

    auto-update on Mon  3 Sep 06:49:40 UTC 2018

--- get-ct-logs.sh
+++ get-ct-logs.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
 log=ct-log.txt
+tf=/tmp/ctget$$.txt
 
 curl "https://crt.sh/?q=%25.onion" |
     perl -nle 'next unless m!TD.*onion!; s!<.*?>! !g; s!(^\s+|\s+$)!!g; print' |
-    sort -u >$log
+    sort -u >$tf
+
+test -s $tf && cp $tf $log
+rm $tf
 
 exit 0
