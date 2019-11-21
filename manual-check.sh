7cf8b47ec28d1b8afa59389540152d2bc4207d2b
Alec Muffett <alec.muffett@gmail.com>
2019-11-21 12:06:53 +0000

    commit: amend quote marks, and add manual-check.sh

---
+++ manual-check.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+SOCKS_PROXY='socks5h://127.0.0.1:9150/'
+USER_AGENT='Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0'
+for url in "$@" ; do
+    echo ":::: $url ::::"
+    curl --head --user-agent "$USER_AGENT" --proxy "$SOCKS_PROXY" "$url" || exit $?
+done
+exit 0
