d453258a7dc33a706271bd91f55a2810533e671b
Alec Muffett <alec.muffett@gmail.com>
2021-06-01 20:38:34 +0000

    commit: last update of ct-log.txt.

--- get-ct-log.sh
+++ get-ct-log.sh
@@ -3,13 +3,13 @@
 log=ct-log.txt
 tf=/tmp/ctget$$.txt
 
-curl "https://crt.sh/?q=%25.onion" |
+curl "https://crt.sh/?q=\.onion" |
     perl -nle 'next unless m!TD.*\.onion\b!; s!\s+!\n!go; s!</?TD>!\n!goi; s!<BR>!\n!goi; print' |
     egrep '[2-7a-z]{56}\.onion$' |
     sort -u |
-    rev |
+    awk -F. '{print $(NF-1), $0}' |
     sort |
-    rev >$tf
+    awk '{print $2}' >$tf
 
 test -s $tf && cp $tf $log
 rm $tf
