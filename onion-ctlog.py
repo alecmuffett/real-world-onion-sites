42bd952c0062df79cb922ef87cfb1147fb6c504a
Alec Muffett <alec.muffett@gmail.com>
2021-07-22 10:26:56 +0000

    auto-update on Thu Jul 22 10:26:56 UTC 2021

--- onion-ctlog.py
+++ onion-ctlog.py
@@ -25,8 +25,7 @@ for tr in table_rows: # skip header
     for br in tr.find_all("br"): br.replace_with(" ")
     td = tr.find_all('td')
     fields = [i.text for i in td]
-    if not fields:
-        continue
+    if not fields: continue
     result = dict()
     result['id'] = fields[0]
     result['at'] = fields[1]
@@ -40,14 +39,16 @@ for tr in table_rows: # skip header
 done = dict()
 for r in results:
     # pprint.pprint(r)
-    stamps = '{0} not_before={1} not_after={2}'.format(r['at'], r['nb'], r['na'])
+    dates = '{0} not_before={1} not_after={2}'.format(r['at'], r['nb'], r['na'])
     sans = r['cn'].split()
+    ca_data = [ x.strip() for x in r['in'].lower().split(',') ]
+    ca = [x for x in ca_data if x.startswith('cn=')][0][3:]
     sans.extend(r['san'].split())
     for san in sans:
         if not onion_re.search(san): continue
         if done.get(san, False): continue
         done[san] = True
         if re.match(r'\*', san):
-            print('* `{san}`\n  * {when}'.format(san=san, when=stamps))
+            print('* `{san}`\n  * {dates} *{ca}*'.format(san=san, dates=dates, ca=ca))
         else:
-            print('* [`{san}`](https://{san})\n  * {when}'.format(san=san, when=stamps))
+            print('* [`{san}`](https://{san})\n  * {dates} *{ca}*'.format(san=san, dates=dates, ca=ca))
